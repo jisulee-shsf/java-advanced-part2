60093a4f0e3a40797e606106eb2f0aef09530ac3
jisulee-shsf <jisulee.shsf@gmail.com>
2026-09-19 00:42:50 +0900

    refactor: [I/O 기본] 문자 다루기 - OutputStreamWriter와 InputStreamReader 사용

---
+++ src/io/text/ReaderWriterMainV2.java
@@ -0,0 +1,31 @@
+package io.text;
+
+import java.io.*;
+
+import static io.text.TextConst.FILE_NAME;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class ReaderWriterMainV2 {
+
+    public static void main(String[] args) throws IOException {
+        String writeString = "ABC";
+
+        // 파일 쓰기
+        FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME);
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, UTF_8);
+        outputStreamWriter.write(writeString);
+        outputStreamWriter.close();
+
+        // 파일 읽기
+        FileInputStream fileInputStream = new FileInputStream(FILE_NAME);
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, UTF_8);
+
+        StringBuilder content = new StringBuilder();
+        int ch;
+        while ((ch = inputStreamReader.read()) != -1) {
+            content.append((char) ch);
+        }
+        inputStreamReader.close();
+        System.out.println(content); // ABC
+    }
+}
